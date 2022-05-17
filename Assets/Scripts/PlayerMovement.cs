015ca4801951de7becd3dd024a53226156bd4d1d
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-05-17 12:07:36 -0300

    Movement nput

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 movement;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        if (transform.eulerAngles.y < 160)
+        {
+            transform.eulerAngles += movement;
+        }
+    }
+}
---
+++ Assets/Scripts/Vector3Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+
+    public Vector3 myVector3;
+    public GameObject myObject;
+    void Start()
+    {
+        myObject.transform.eulerAngles = myVector3;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
